fcd11ac74189e7c7497c59f1838e2a1b314dfa21
Carson Thackston <carson.thackston@gamil.com>
2020-12-09 08:52:25 -0500

    Made fortune 1 and 2 work. Also made durability work through minecraft's system.

--- src/main/java/com/github/carthax08/morefortune/events/BlockBreakEventHandeler.java
+++ src/main/java/com/github/carthax08/morefortune/events/BlockBreakEventHandeler.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 
 import java.util.Random;
 
@@ -23,19 +24,19 @@ public class BlockBreakEventHandeler implements Listener {
             if(Morefortune.getConfigFile().getBoolean("settings.EnableNetherite")){
                 ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
                 if(item.getItemMeta().hasEnchant(Enchantment.LOOT_BONUS_BLOCKS)){
-                    e.setCancelled(true);
                     int level = item.getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS);
-                    Util.handleFortune(random, level, e.getPlayer().getWorld(), e.getBlock(), e.getPlayer());
+                    Damageable damageable = (Damageable) item.getItemMeta();
+                        Util.handleFortune(random, level, e.getPlayer().getWorld(), e.getBlock(), e.getPlayer());
                 }
             }
         }else if(e.getBlock().getType().equals(Material.CLAY)){
             if(Morefortune.getConfigFile().getBoolean("settings.EnableClay")){
                 ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
                 if(item.getItemMeta().hasEnchant(Enchantment.LOOT_BONUS_BLOCKS)){
-                    e.setCancelled(true);
+                    Damageable damageable = (Damageable) item.getItemMeta();
                     int level = item.getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS);
                     e.getPlayer().sendMessage(String.valueOf(level));
-                    Util.handleFortune(random, level, e.getPlayer().getWorld(), e.getBlock(), e.getPlayer());
+                        Util.handleFortune(random, level, e.getPlayer().getWorld(), e.getBlock(), e.getPlayer());
                 }
             }
         }
--- src/main/java/com/github/carthax08/morefortune/util/Util.java
+++ src/main/java/com/github/carthax08/morefortune/util/Util.java
@@ -5,46 +5,82 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Random;
 
 public class Util {
-    public static void handleFortune(Random random, int level, World world, Block block, Player player){
-        if(level == 1){
+    public static void handleFortune(Random random, int level, World world, Block block, Player player) {
+        if (level == 1) {
             int r = random.nextInt(100);
-        }else if(level == 2){
-            int r = random.nextInt(100);
-        }else if(level == 3){
-            int r = random.nextInt(100);
-            if(r <= 40){
+            if(r <= 66){
                 if(block.getType().equals(Material.CLAY)){
-                    ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 4);
+                    ItemStack itemStack = new ItemStack(Material.CLAY_BALL,4);
                     world.dropItemNaturally(block.getLocation(), itemStack);
-                }else {
-                    ItemStack itemStack = new ItemStack(block.getType(), 1);
+                }else{
+                    ItemStack itemStack = new ItemStack(block.getType());
                     world.dropItemNaturally(block.getLocation(), itemStack);
                 }
-            }else if(r <= 60){
+            }else if(r <= 100){
+                if(block.getType().equals(Material.CLAY)) {
+                    ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 8);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }else{
+                    ItemStack itemStack = new ItemStack(block.getType(),2);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }
+            }
+        } else if (level == 2) {
+            int r = random.nextInt(100);
+            if(r <= 50){
                 if(block.getType().equals(Material.CLAY)){
+                    ItemStack itemStack = new ItemStack(Material.CLAY_BALL,4);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }else{
+                    ItemStack itemStack = new ItemStack(block.getType());
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }
+            }else if(r <= 75){
+                if(block.getType().equals(Material.CLAY)) {
+                    ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 8);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }else{
+                    ItemStack itemStack = new ItemStack(block.getType(),2);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }
+            }else if(r <= 100){
+                if(block.getType().equals(Material.CLAY)) {
+                    ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 12);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }else{
+                    ItemStack itemStack = new ItemStack(block.getType(),3);
+                    world.dropItemNaturally(block.getLocation(), itemStack);
+                }
+            }
+        } else if (level == 3) {
+            int r = random.nextInt(100);
+            if (r <= 60 && r > 40) {
+                if (block.getType().equals(Material.CLAY)) {
                     ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 4);
                     itemStack.setAmount(itemStack.getAmount() * 2);
                     world.dropItemNaturally(block.getLocation(), itemStack);
-                }else {
+                } else {
                     ItemStack itemStack = new ItemStack(block.getType(), 1);
                     itemStack.setAmount(itemStack.getAmount() * 2);
                     world.dropItemNaturally(block.getLocation(), itemStack);
                 }
-            }else if(r <= 80){
-                if(block.getType().equals(Material.CLAY)){
+            } else if (r <= 80) {
+                if (block.getType().equals(Material.CLAY)) {
                     ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 4);
                     itemStack.setAmount(itemStack.getAmount() * 3);
                     world.dropItemNaturally(block.getLocation(), itemStack);
-                }else {
+                } else {
                     ItemStack itemStack = new ItemStack(block.getType(), 1);
                     itemStack.setAmount(itemStack.getAmount() * 3);
                     world.dropItemNaturally(block.getLocation(), itemStack);
                 }
-            }else if(r <= 100) {
+            } else if (r <= 100) {
                 if (block.getType().equals(Material.CLAY)) {
                     ItemStack itemStack = new ItemStack(Material.CLAY_BALL, 4);
                     itemStack.setAmount(itemStack.getAmount() * 4);
@@ -55,7 +91,7 @@ public class Util {
                     world.dropItemNaturally(block.getLocation(), itemStack);
                 }
             }
-        }else{
+        } else {
             System.out.println(player.getName() + " has used a fortune level above 3!");
             player.sendMessage("Your fortune level is above three!");
         }
